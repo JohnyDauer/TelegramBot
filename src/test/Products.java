3febb3d480f2326c6b2a2e5bff5945249e655d5a
cerep89 <viorel.popa1989@gmail.com>
2016-09-10 03:28:35 +0300

    Create class products

---
+++ src/test/Products.java
@@ -0,0 +1,53 @@
+package test;
+
+/**
+ * Created by Viorel on 10.09.2016.
+ */
+public class Products {
+    public int id;
+    public String name;
+    public String category;
+    public double price;
+
+    public Products(int id, String name, String category, double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Products() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
